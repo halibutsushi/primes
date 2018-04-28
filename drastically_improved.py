85587e959f66db8ce94cc41e6ce9c8e34570d11d
kr2842 <kr2842@gmail.com>
2018-04-29 00:23:48 +0900

    find primes efficiently

---
+++ drastically_improved.py
@@ -0,0 +1,34 @@
+
+
+if __name__ == "__main__":
+    prime_list = []
+    results = []
+    n = 1000000
+    #n = 1000
+
+    for i in range(n):
+        if i == 0 or i == 1:
+            results.append((i, False))
+            continue
+
+        end_limit = i
+        is_decided = False
+        for prime in prime_list:
+            if i % prime == 0:
+                results.append((i, False))
+                is_decided = True
+                break
+
+            if prime > end_limit:
+                results.append((i, True))
+                prime_list.append(i)
+                is_decided = True
+                break
+
+            end_limit = i // prime + 1
+
+        if not is_decided:
+            results.append((i, True))
+            prime_list.append(i)
+
+    print(results[:-100])
