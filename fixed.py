886be06d7bf8a78b993e98301b47d23a19be524e
kr2842 <kr2842@gmail.com>
2018-04-28 23:47:51 +0900

    output que is not instace variable

--- fixed.py
+++ fixed.py
@@ -4,13 +4,14 @@ import pyprimes
 class Worker:
 
     def __init__(self, in_qu, out_q):
-        self.q = in_q
-        self.proc = mp.Process(target=self._is_prime, args=(out_q,))
+        self.in_q = in_q
+        self.out_q = out_q
+        self.proc = mp.Process(target=self._is_prime)
 
-    def _is_prime(self, out_q):
-        while not self.q.empty():
-            elt = self.q.get()
-            out_q.put((elt, pyprimes.isprime(elt)))
+    def _is_prime(self):
+        while not self.in_q.empty():
+            elt = self.in_q.get()
+            self.out_q.put((elt, pyprimes.isprime(elt)))
 
     def start(self):
         self.proc.start()
@@ -23,7 +24,7 @@ if __name__ == "__main__":
     out_q = mp.SimpleQueue()
     workers = [Worker(in_q, out_q) for _ in range(4)]
 
-    n = 1000000
+    n = 100
     for v in range(n):
         in_q.put(v)
 
